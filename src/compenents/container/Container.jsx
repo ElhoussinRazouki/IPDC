57cc05a80aa42396bc7f879594f277424760eed3
Mansouri06 <klord9447@gmail.com>
2024-03-09 23:19:42 +0100

    update

--- src/compenents/container/Container.jsx
+++ src/compenents/container/Container.jsx
@@ -1,6 +1,8 @@
 import familyImg from '../../assets/family.png'
 import Button from "../button/Button"
 import car from "../../assets/car.png"
+import house from "../../assets/house.png"
+
 const Container = () => {
   return (
     <>
@@ -16,20 +18,20 @@ const Container = () => {
   Car Loans
   </button>
 </div>
-<div className='flex justify-between'>
-<div className='pl-[135px]'>
-  <h1 className='font-[metropolis] max-w-[453px] text-[63px] font-extrabold'>
+<div className='flex justify-between pr-12'>
+<div className='pl-[135px] space-y-10'>
+  <h1 className='font-[metropolis-bold] max-w-[453px] text-[63px] font-extrabold leading-[68px]'>
   Chase Your
-Dream with us
+ Dream with us
   </h1>
-  <p>The harder you work for something, the greater you’ll feel when you achieve it.</p>
+  <p className='max-w-[380px] font-[metropolis] text-[--gray-color]'>The harder you work for something, the greater you’ll feel when you achieve it.</p>
   <div>
     <Button value="Apply Online"/>
     <button className='outline-none border-solid '></button>
   </div>
 </div>
 <button className='flex  w-[191px] bg-white h-[71px]  font-[metropolis] justify-center items-center rounded-[55px]  shadow-xl mt-7 ml-[27px] outline-none ' >
-    <img src={car} alt="" className='w-[25.54px] mr-4 mr-5 ' />
+    <img src={house} alt="" className='w-[25.54px] mr-5 ' />
     Home Loan
   </button>
 
