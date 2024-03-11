2e6618be292ffd92bf8ccd9e02b2e14ae278dc2b
Mansouri06 <klord9447@gmail.com>
2024-03-11 13:29:38 +0000

    #

--- src/compenents/container/Container.jsx
+++ src/compenents/container/Container.jsx
@@ -35,20 +35,20 @@ const Container = () => {
     <div>
       <div className=" absolute right-0 top-0 h-[682px] w-[50%] z-[-1] bg-[#FFEAF5] Pt-20"></div>
       <div>
-        <div>
+        <div className="flex justify-end z-0">
           <img
             src={familyImg}
             alt=""
-            className="absolute h-[690px] w-[62%] top-28 right-0"
+            className="absolute h-[690px] w-[90%] md:w-[62%] md:top-28 md:right-0 "
           />
         </div>
-        <div className="w-full flex justify-center ">
-          <button className="flex  w-[191px] bg-white h-[71px] justify-center items-center rounded-[55px]  shadow-2xl -mr-5 mt-7 ml-[27px] outline-none ">
+        <div className="w-full flex md:justify-center justify-start z-10 ">
+          <button className="flex  w-[191px] bg-white h-[71px]  justify-center items-center rounded-[55px]  shadow-2xl -mr-5 mt-7 ml-[27px] outline-none ">
             <img src={car} alt="" className="w-[25.54px] mr-4 mr-5 " />
             Car Loans
           </button>
         </div>
-        <div className="flex justify-between pr-12">
+        <div className="flex justify-between md:pr-12">
           <div className="pl-[135px] space-y-10">
             <h1 className="font-[metropolis-bold] max-w-[453px] text-[63px] font-extrabold leading-[68px]">
               Chase Your Dream with us
